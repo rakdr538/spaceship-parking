91a8100bc0dd09116420d17b1545795abc2b3cee
Rakesh D <rakdr538@gmail.com>
2024-02-12 16:58:49 +0100

    Fixed test containers.

--- src/test/java/com/twoday/spaceshipparking/SpaceShipParkingControllerIT.java
+++ src/test/java/com/twoday/spaceshipparking/SpaceShipParkingControllerIT.java
@@ -10,9 +10,7 @@ import com.twoday.spaceshipparking.dto.CreateParkingRequestDTO;
 import com.twoday.spaceshipparking.service.ParkingService;
 import com.twoday.spaceshipparking.usecases.GetParkedSpaceShipUseCase;
 import com.twoday.spaceshipparking.usecases.ParkSpaceShipUseCase;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EmptySource;
@@ -23,36 +21,50 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SpaceShipParkingControllerIT {
 
-    public static final String BASE_URL = "/api/v1/parkings";
+    private static final String BASE_URL = "/api/v1/parkings";
     @LocalServerPort
     private int port;
+    private static final MongoDBContainer mongoDBContainer;
+
+    static {
+        mongoDBContainer = new MongoDBContainer("mongo:latest").withExposedPorts(27017);
+        mongoDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Autowired
     private TestRestTemplate restTemplate;
-
     @Autowired
     private ParkingRepository parkingRepository;
-
     @Autowired
     private ParkingService parkingService;
-
     @Autowired
     private GetParkedSpaceShipUseCase getParkedSpaceShipUseCase;
-
     @Autowired
     private ParkSpaceShipUseCase parkSpaceShipUseCase;
 
     private static HttpHeaders headers;
-
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeAll
@@ -60,22 +72,53 @@ class SpaceShipParkingControllerIT {
         headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
     }
+
     private String createURLWithPort() {
         return "http://localhost:" + port + BASE_URL;
     }
 
-    @Nested
-    class ValidArgumentTests {
+    @Test
+    void givenValidCreateReq_thenCreateNewParkingAndDoGetAndUpdateAfter2Mins() throws InterruptedException {
+        final List<Parking> parkings = new ArrayList<>();
 
-        //List<CreateParkingRequestDTO> requestDTOS = createValidRequests();
+        // create
+        requestDTOS().forEach(reqDTO -> {
+            HttpEntity<String> entity = new HttpEntity<>(reqDTO, headers);
+            ResponseEntity<Parking> response = restTemplate.exchange(
+                    createURLWithPort(), HttpMethod.POST, entity, Parking.class);
+            if (response.getStatusCode().is2xxSuccessful()) {
+                parkings.add(response.getBody());
+            }
+        });
 
+        // get
+        parkings.forEach(parking -> {
+            var response = restTemplate
+                    .getForEntity(createURLWithPort() + "/" + parking.getId(), Parking.class);
+            assertTrue(response.getStatusCode().is2xxSuccessful(), "Should be 200");
+            assertNotNull(response.getBody(), "Body is not suppose to be null");
+            assertEquals(parking, response.getBody());
+        });
+
+        // update after 2mins
+        TimeUnit.MINUTES.sleep(2);
+        if (parkings.isEmpty()) {
+            fail("Parkings cannot be empty");
+        }
+
+        ResponseEntity<Parking> getParked = restTemplate.getForEntity(
+                createURLWithPort() + "/vacate/" + parkings.getFirst().getId(), Parking.class);
+        assertTrue(getParked.getStatusCode().is2xxSuccessful(), "Should be 200");
+        assertNotNull(getParked.getBody(), "Body is not suppose to be null");
+        assertNotNull(getParked.getBody().getParkingEndedAt());
+        assertEquals(1, getParked.getBody().getDurationInHours());
     }
 
     @Nested
     class InvalidArgumentTests {
 
         @Test
-        void whenInValidCreateReq1_thenBadReqResponse () throws Exception {
+        void whenInValidCreateReq1_thenBadReqResponse() throws Exception {
             final CreateParkingRequestDTO badRequestDTO = CreateParkingRequestDTO.builder()
                     .parkingPlace(ParkingPlace.builder().floor(-1).plot(25).build())
                     .spaceShip(SpaceShip.builder().name(null).registrationNumber(null).build())
@@ -92,7 +135,7 @@ class SpaceShipParkingControllerIT {
         }
 
         @Test
-        void whenInValidCreateReq2_thenBadReqResponse () throws Exception {
+        void whenInValidCreateReq2_thenBadReqResponse() throws Exception {
             final CreateParkingRequestDTO badRequestDTO = CreateParkingRequestDTO.builder()
                     .parkingPlace(ParkingPlace.builder().floor(2).plot(14).build())
                     .spaceShip(SpaceShip.builder().name(null).registrationNumber("@mine_own").build())
@@ -100,7 +143,6 @@ class SpaceShipParkingControllerIT {
                     .build();
 
             final String st1 = objectMapper.writeValueAsString(badRequestDTO);
-            System.out.println(st1);
 
             HttpEntity<String> entity = new HttpEntity<>(st1, headers);
             ResponseEntity<Parking> response = restTemplate.exchange(
@@ -113,7 +155,7 @@ class SpaceShipParkingControllerIT {
 
         @ParameterizedTest
         @CsvSource({"-2,16", "4,14", "0,0"})
-        void whenValidCreateReq_thenBadReqResponse (Integer floor, Integer plot) throws Exception {
+        void whenValidCreateReq_thenBadReqResponse(Integer floor, Integer plot) throws Exception {
             // boundary value testing
             HttpEntity<String> entity = new HttpEntity<>(getReq(floor, plot, "CSX 123"), headers);
             ResponseEntity<Parking> response = restTemplate.exchange(
@@ -153,38 +195,6 @@ class SpaceShipParkingControllerIT {
         }
     }
 
-    @Nested
-    class UseCaseTests {
-        @Test
-        void givenValidCreateReq_thenCreateNewParkingAndDoGet () throws JsonProcessingException {
-            HttpEntity<String> entity = new HttpEntity<>(getReq(2,5, "CSX 123"), headers);
-            ResponseEntity<Parking> response = restTemplate.exchange(
-                    createURLWithPort(), HttpMethod.POST, entity, Parking.class);
-            assertTrue(response.getStatusCode().is2xxSuccessful(), "Should be 201");
-            var newParking = response.getBody();
-            assertNotNull(newParking, "Body is not suppose to be null");
-            assertNotNull(newParking.getId(), "Should get an ID");
-
-            entity = new HttpEntity<>(null, headers);
-            response = restTemplate
-                    .getForEntity(createURLWithPort() + "/" + newParking.getId(), Parking.class);
-            assertTrue(response.getStatusCode().is2xxSuccessful(), "Should be 200");
-            assertNotNull(response.getBody(), "Body is not suppose to be null");
-            assertEquals(newParking, response.getBody());
-        }
-
-        @Test
-        void givenValidDBId_thenUpdateParking() {
-            HttpEntity<String> entity = new HttpEntity<>(null, headers);
-            ResponseEntity<Parking> getParked = restTemplate.getForEntity(
-                    createURLWithPort() + "/vacate/" + "65c4d41830e446024202fdbc",
-                        Parking.class);
-            assertTrue(getParked.getStatusCode().is2xxSuccessful(), "Should be 200");
-            assertNotNull(getParked.getBody(), "Body is not suppose to be null");
-
-        }
-    }
-
     private String getReq(int floor, int plot, String regNo) throws JsonProcessingException {
         final CreateParkingRequestDTO requestDTO = CreateParkingRequestDTO.builder()
                 .parkingPlace(ParkingPlace.builder().floor(floor).plot(plot).build())
@@ -193,5 +203,26 @@ class SpaceShipParkingControllerIT {
                 .build();
 
         return objectMapper.writeValueAsString(requestDTO);
+    }
+
+    private int getRandomNumberUsingNextInt(int max) {
+        Random random = new Random();
+        return random.nextInt(max - 1) + 1;
+    }
+
+    private List<String> requestDTOS() {
+        var createParkingRequestDTOs = new ArrayList<String>();
+
+        IntStream.range(0, 9).forEach(i -> {
+            var floor = getRandomNumberUsingNextInt(3);
+            var plot = getRandomNumberUsingNextInt(15);
+            var regNo = "CSV" + floor + plot + i;
+            try {
+                createParkingRequestDTOs.add(getReq(floor, plot, regNo));
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return createParkingRequestDTOs;
     }
 }
